1dae6d86a11191ee36ea9cda15178059b7f4ffcf
hobgroblin <soupandcrackers@gmail.com>
2015-06-04 00:58:21 -0400

    Broken code!
    By not properly bringing in variables in to the definitions, I
    effectively broke the code. no good!

--- OBJ_inspect.py
+++ OBJ_inspect.py
@@ -25,10 +25,10 @@ class LoadOBJ ():
 #        self.uv_bounding_box_max = uv_bounding_box_max #2 float variable list
 #        self.uv_bounding_box_min = uv_bounding_box_min #2 float variable list
 #        self.truncated_OBJ = truncated_OBJ # text file
-#        self.vertices_count = vertices_count
-#        self.faces_count = faces_count
-#        self.uv_vertices_count = uv_vertices_count
-#        self.vertex_normals_count = vertex_normals_count
+        self.vertex_count = 0
+        self.face_count = 0
+        self.uv_vertex_count = 0
+        self.vertex_normal_count = 0
 #        self.other_csv_count = other_csv_count
 #        self.other_csv_names = other_csv_names
 #        self.objects = objects # list of stirngs
@@ -48,38 +48,80 @@ class LoadOBJ ():
         #set up neceesary varibles
         current_line_type = 'none'
         pervious_line_type = 'none'
-        geometry_line_type = 'none'
+        is_line_type_geometry = False
         geometry_line_type_repeated = 0
         obj_file = open(obj_path, 'r') #open file in read mode
+        has_vertex_count_flag_come_up = False
+        has_vertex_normal_flag_come_up = False
+        has_face_flag_come_up = False
+        has_uv_vertex_come_up = False
 
+        def reset_all_has_geometry_type_flag_come_up():
+            has_vertex_count_flag_come_up = False
+            has_vertex_normal_flag_come_up = False
+            has_face_flag_come_up = False
+            has_uv_vertex_come_up = False
         #functions to act on each line type
+        #TODO should the functions be in a different file?
         def comment():
             geometry_line_type_repeated = 0
+            is_line_type_geometry = False
             print 'comment'
+            reset_all_has_geometry_type_flag_come_up
         def mtllib():
             geometry_line_type_repeated = 0
+            is_line_type_geometry = False
             print 'mtlib'
+            reset_all_has_geometry_type_flag_come_up
         def usemtl():
             geometry_line_type_repeated = 0
+            is_line_type_geometry = False
             print 'usemtl'
+            reset_all_has_geometry_type_flag_come_up
         def geo_object():
             geometry_line_type_repeated = 0
+            is_line_type_geometry = False
             print 'geo_object'
+            reset_all_has_geometry_type_flag_come_up
         def group():
             geometry_line_type_repeated = 0
+            is_line_type_geometry = False
+            reset_all_has_geometry_type_flag_come_up
         def s():
             geometry_line_type_repeated = 0
+            is_line_type_geometry = False
+            reset_all_has_geometry_type_flag_come_up
+## Geometry flags have a lot of repeating features, can this be compressed
+## and remain readable?
         def v():
-            pass
+            self.vertex_count += 1
+            is_line_type_geometry = True
+            global has_vertex_count_flag_come_up
+            if has_vertex_count_flag_come_up == False:
+                truncated_obj_file.write(line)
+                has_vertex_count_flag_come_up = True
         def vn():
-            pass
+            self.vertex_normal_count += 1
+            is_line_type_geometry = True
+            if has_vertex_normal_flag_come_up == False:
+                truncated_obj_file.write(line)
+                has_vertex_normal_flag_come_up = True
         def f():
-            pass
+            self.face_count += 1
+            is_line_type_geometry = True
+            if has_face_flag_come_up == False:
+                truncated_obj_file.write(line)
+                has_face_flag_come_up = True
         def vt():
-            pass
+            self.uv_vertex_count += 1
+            is_line_type_geometry = True
+            if has_uv_vertex_come_up == False:
+                truncated_obj_file.write(line)
+                has_uv_vertex_come_up = True
         def new_flag_type():
             print 'new flag type found:'
             print current_line_type
+
         #dictionary to call functions based on line flag
         line_flag_function_dictionary = {
             '#': comment,
@@ -99,8 +141,8 @@ class LoadOBJ ():
             #find the type of the current line
             #by spliting the line on whitespace and calling the first element
             current_line_type = line.split()[0]
+
             #call the function based on the line type
-            #TODO should the functions be in a different file?
             line_flag_function_dictionary.get(current_line_type,new_flag_type)()
 
 
@@ -113,10 +155,10 @@ class LoadOBJ ():
             #for values > 3, thus skipping the other 2 steps most of the tiem
             #even better don't increment past a certain poirnt
             #save the add and possibly more expensive comparison?
-            if geometry_line_type_repeated < 3:
-                truncated_obj_file.write(line)
-            elif geometry_line_type_repeated == 3:
-                truncated_obj_file.write('...\n')
+#            if geometry_line_type_repeated < 3:
+#                truncated_obj_file.write(line)
+#            elif geometry_line_type_repeated == 3:
+#                truncated_obj_file.write('...\n')
 
 
 
@@ -137,21 +179,21 @@ class LoadOBJ ():
 ##456 face (f) lines
 ##345 vertice (v) lines
             #figure out if we need to increase geometry_line_type_repeated
-            if current_line_type == pervious_line_type:
-                geometry_line_type_repeated += 1
+#            if current_line_type == pervious_line_type:
+#                geometry_line_type_repeated += 1
 #                print 'line same as previous'
 #                print geometry_line_type_repeated
-            else:
-                geometry_line_type_repeated = 0
+#            else:
+#                geometry_line_type_repeated = 0
 #                    print current_line_type
 #                    print pervious_line_type
-                print 'no repeat'
+#                print 'no repeat'
 
             pervious_line_type = current_line_type
 
 
         obj_file.close
         truncated_obj_file.close
-
+        print self.faces_count
 #obj_file_path = 'D:\OBJ test files for OBJinspect\MeshLab\MeshLabgreekSlave.final.div1.obj'
 #obj_file_object = OBJ_inspect.LoadOBJ(obj_path)
